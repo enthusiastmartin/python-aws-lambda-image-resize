5bcc62cc3a5f3b1d420c326da6b9649c051ee657
Martin <martin.hloska@gmail.com>
2017-06-09 17:13:41 +0200

    improved code comments

--- resize_image.py
+++ resize_image.py
@@ -20,6 +20,8 @@ TARGET_RESIZE_SIZE = [400, 400]
 
 
 class S3MockClient(object):
+    """Mock class for testing purposes"""
+
     def download_file(self, *args):
         logging.debug("Downloading file from s3 -  {bucket}/{key}".format(bucket=args[0], key=args[1]))
         import shutil
@@ -30,31 +32,60 @@ class S3MockClient(object):
 
 
 class S3MockResrouce(object):
+    """Mock class for testing purposes"""
 
     class ObjectAclMock(object):
+        """Mock class for testing purposes"""
+
         def put(self, *args, **kwargs):
             pass
 
-    def ObjectAcl(self, *args):
+    @staticmethod
+    def ObjectAcl(*args):
         return S3MockResrouce.ObjectAclMock()
 
 
 def connect_s3():
+    """Connect to S3
+    If DEBUG: uses mock object for s3 client and resource
+    :return: 
+    """
     if DEBUG:
         return {'client': S3MockClient(), 'resource': S3MockResrouce()}
     return {'client': boto3.client('s3'), 'resource': boto3.resource('s3')}
 
 
 def process_image(s3_conn, local_source_path, local_resize_path, desired_size, s3_path):
+    """Process image - open, resize, save locally and upload to S3
+    :param s3_conn: s3 connection dict
+    :param local_source_path: path to local original file
+    :param local_resize_path: output path for resized image
+    :param desired_size: desized size, format ex: [400,400]
+    :param s3_path: S3 location excluding bucket
+    :return: 
+    """
 
+    #
+    # Open original file and resize it
+    #
     with Image.open(local_source_path) as image:
         resized_image = resizeimage.resize_cover(image, desired_size)
         resized_image.save(local_resize_path, image.format)
 
+    #
+    # Upload resized image to S3
+    #
     s3_conn['client'].upload_file(local_resize_path, '{bucket_name}'.format(bucket_name=TARGET_BuCKET_NAME), s3_path)
+
+    #
+    # Set ACL - public-read in this case
+    #
     object_acl = s3_conn['resource'].ObjectAcl('{bucket_name}'.format(bucket_name=TARGET_BuCKET_NAME), s3_path)
     response = object_acl.put(ACL='public-read')
 
+    #
+    # Maybe log response
+    #
     if DEBUG:
         logging.debug(response)
     else:
@@ -63,20 +94,41 @@ def process_image(s3_conn, local_source_path, local_resize_path, desired_size, s
 
 
 def handler(event, context):
+    """AWS Lambda main handler
+    
+    Entry point for the function
+    
+    :param event: event data
+    :param context: runtime information
+    :return: 
+    """
+
+    #
+    # Connect to S3
+    #
     s3_conn = connect_s3()
 
+    #
+    # Process record from event data
+    #
     for record in event['Records']:
         logging.debug('Processing {}'.format(str(record)))
         bucket = record['s3']['bucket']['name']
 
         key = record['s3']['object']['key']
 
+        #
+        # Construct temporary location for original , resized image and target S3 location
+        #
         download_path = '/tmp/orig-{}_orig'.format(uuid.uuid4())
         local_resize_location = '/tmp/resized-{}'.format(uuid.uuid4())
 
-        s3_conn['client'].download_file(bucket, key, download_path)
-
         s3_location = TARGET_BUCKET_LOCATION.format(filename=str(os.path.basename(key)))
+
+        #
+        # Download file
+        #
+        s3_conn['client'].download_file(bucket, key, download_path)
 
         try:
             process_image(s3_conn, download_path, local_resize_location, TARGET_RESIZE_SIZE, s3_location)
@@ -89,6 +141,9 @@ def handler(event, context):
             logging.error(str(e))
             traceback.print_exc(file=sys.stderr)
         finally:
+            #
+            # Delete temporary files
+            #
             logging.info("Cleaning up")
 
             try:
